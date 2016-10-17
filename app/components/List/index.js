d7e8dbe69acb928068b4256d4e936cefd985b7e3
bitaru <aiir.diir@gmail.com>
2016-10-17 10:32:09 +0300

    Add listenening functionality

--- app/components/List/index.js
+++ app/components/List/index.js
@@ -1,17 +1,54 @@
-import { compose } from 'recompose';
+import { compose, withHandlers, withProps } from 'recompose';
 import { connect } from 'react-redux';
+import cx from 'classnames';
+import Events from '../../lib/EventBus';
 
 import styles from './styles.css';
+import RecordIcon from '../icons/record';
+import PlayIcon from '../icons/play';
 
-const Room = ({ image, name, tags }) => (
-  <li className={styles.room}>
-    <div className={styles.avatar} style={{ backgroundImage: `url(${image})` }} />
+const Room = compose(
+  withProps(props => ({
+    avaible: !(props.isOwner || props.isListener)
+  })),
+  withHandlers({
+    onClick: props => e => {
+      e.preventDefault();
+      if (props.isOwner || props.isListener) return;
+      Events.emit('joinRoom', { id: props.id });
+    }
+  })
+)(({ avaible, image, name, tags, isOwner, isListener, onClick }) => (
+  <li
+    onClick={onClick}
+    className={cx(
+      styles.room,
+      isOwner && styles.owner,
+      isListener && styles.listener,
+      avaible && styles.avaible
+    )}>
+    <div className={styles.avatar} style={{ backgroundImage: `url(${image})` }}>
+      <div className={styles.statusWrap}>
+        {isOwner && <div className={styles.statusIcon}><RecordIcon /></div>}
+        {isListener && <div className={styles.statusIcon}><PlayIcon /></div>}
+      </div>
+    </div>
     <div className={styles.body}>
       <h3>{name}</h3>
       {tags && <p>{tags}</p>}
     </div>
+    <div className={styles.overlay}>
+      {isOwner && <p className={styles.text}>Your stream</p>}
+      {isListener && <p className={styles.text}>Current stream</p>}
+    </div>
+    {
+      avaible &&
+        <div className={styles.avaibleOverlay}>
+          <p className={styles.text}>Join this Room</p>
+        </div>
+    }
   </li>
-);
+));
 
 export default compose(
   connect(({ rooms = [] }) => ({ rooms }))
